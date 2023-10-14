3a68f76e649e3f00f1ca2f6817afb0e65d0c1e78
Vurgun <you@example.comslayer13_86@mail.ru>
2023-10-15 00:47:10 +0900

    if else

---
+++ Exam5_ifel/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введи имя: ");
+string username = Console.ReadLine ();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Урв, это МАША");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
